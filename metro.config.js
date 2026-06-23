d6dd051a5e2ec5342ff09a094bb6adf0d2712252
zedspace10 <t5y9dzjz6m@privaterelay.appleid.com>
2026-06-23 17:37:10 +0100

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -3,7 +3,7 @@ const { getDefaultConfig } = require("expo/metro-config");
 const config = getDefaultConfig(__dirname);
 
 config.resolver.extraNodeModules = {
-  buffer: require.resolve("buffer/"),
+  buffer: require.resolve("buffer"),
 };
 
 module.exports = config;
